fdff4709c7a9c14fa22e35e118cca2998cff5c5e
Pascual Lorente <pasculorente@gmail.com>
2021-05-03 09:14:16 +0100

    Created build script for JAR and DEB

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
-/home/pascual/.jdks/openjdk-15.0.2/bin/jpackage \
+#!/bin/bash
+# This will create target/vcf-viewer-{version}-jar-with-dependencies.jar
+mvn clean compile assembly:single
+
+# Add all dependencies (as jars) to target/jmods
+mvn package
+mvn dependency:copy-dependencies -DoutputDirectory=target/jmods/
+
+# Create vcf-viewer_1.0-1_amd64.deb (or exe in windows)
+# jpackage v15 is needed
+jpackage \
 	--input target/jmods/ \
 	--main-jar vcf-viewer-1.0-SNAPSHOT.jar \
 	--main-class org.uichuimi.variant.viewer.Main \
